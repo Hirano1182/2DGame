103b4b68cd1c0cb6bbaed5976f6c72d39035de52
Hirano1182 <base09@outlook.jp>
2021-08-14 09:10:53 +0900

    g

--- 2D Game Project/Assets/scripts/scripts/group10_Other/Iffalled_GameOver.cs
+++ 2D Game Project/Assets/scripts/scripts/group10_Other/Iffalled_GameOver.cs
@@ -1,17 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Iffalled_GameOver : MonoBehaviour
 {
     public string ObjectName1;
+    public string ObjectName2;
+    public string ObjectName3;
     public string targetObjectName;
+    
     GameObject showObject1;
+    GameObject showObject2;
+    GameObject showObject3;
     // Start is called before the first frame update
     void Start()
     {
         showObject1 = GameObject.Find(ObjectName1);
         showObject1.SetActive(false);
+        showObject2 = GameObject.Find(ObjectName2);
+        showObject2.SetActive(false);
+        showObject3 = GameObject.Find(ObjectName3);
+        
+        Time.timeScale = 1;
     }
 
     // Update is called once per frame
@@ -19,7 +30,23 @@ public class Iffalled_GameOver : MonoBehaviour
     {
         if (collision.gameObject.name == targetObjectName)
         {
-            showObject1.SetActive(true);
+            showObject3.SetActive(true);
+            Invoke(nameof(Damage), 3.0f);
+
         }
     }
+
+    void Damage()
+    {
+        showObject1.SetActive(true);
+        showObject2.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    void OnDestroy()
+    {
+        CancelInvoke();
+    }
+
+
 }
