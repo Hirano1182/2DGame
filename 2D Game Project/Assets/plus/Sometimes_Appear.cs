c3e902685fa716ca5a21a70efa1618fe30e0f646
MK-dev48 <85719380+MK-dev48@users.noreply.github.com>
2021-09-10 04:25:07 +0900

    2021-09-10

---
+++ 2D Game Project/Assets/plus/Sometimes_Appear.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sometimes_Appear : MonoBehaviour
+{
+    public string targetName;
+    public int Max_Count;
+
+    GameObject Target;
+
+    int count;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 0;
+        Target = GameObject.Find(targetName);
+        Target.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        count = count + 1; // カウンターに1を足して
+        if (count >= Max_Count)
+        { // もし、maxCountになったら
+            Invoke(nameof(T_Active), 3.5f); // 回転して曲がる
+            count = 0; // カウンターをリセット
+            Target.SetActive(false);
+        }
+    }
+
+    private void T_Active()
+    {
+        Target.SetActive(true);
+    }
+}
