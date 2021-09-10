8e5af3ad5cceb3f1f80d7b72c1e72838d95c1ddc
kaede1028 <88543491+kaede1028@users.noreply.github.com>
2021-09-10 22:50:06 +0900

    20210910

--- 2D Game Project/Assets/scripts/scripts/maincharascript 1/gameclear_and_stop_changescene.cs
+++ 2D Game Project/Assets/scripts/scripts/maincharascript 1/gameclear_and_stop_changescene.cs
@@ -3,21 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class gameclear_and_changescene : MonoBehaviour
+public class gameclear_and_stop_changescene : MonoBehaviour
 {
     public string target;
     public string sceneName;
-    public string objectname;
+    public string targetobjectname1;
+    public string targetobjectname3;
+    public string targetobjectname4;
     public int check;
-    GameObject gameObject1;
+    GameObject gameObject1; 
+    GameObject gameObject3;
+    GameObject gameObject4;
 
 
     Vector3 base_pos;
 
     void Start()
     {
-        gameObject1 = GameObject.Find(objectname);
-       
+        gameObject1 = GameObject.Find(targetobjectname1);
+        gameObject3 = GameObject.Find(targetobjectname3);
+        gameObject4 = GameObject.Find(targetobjectname4);
+
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -25,7 +31,9 @@ public class gameclear_and_changescene : MonoBehaviour
         if(collision.gameObject.name == target)
         {
             check++;
-           gameObject1.GetComponent<OnKeyPress_MoveGravityPlus>().enabled = false;
+            gameObject1.GetComponent<Forever_MoveH>().enabled = false;
+            gameObject3.GetComponent<Forever_MoveH>().enabled = false;
+            gameObject4.GetComponent<OnKeyPress_MoveGravityPlus>().enabled = false;
             Invoke(nameof(Damage), 3.0f);
 
         }
